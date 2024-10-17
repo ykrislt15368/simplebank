183cf40555516a5ecc00c502ba4a7406689b4d04
Krishna Yelleti <ykrislt15368@gmail.com>
2024-10-17 09:08:18 +0000

    create user api developed previous is create accnt

--- db/sqlc/user_test.go
+++ db/sqlc/user_test.go
@@ -7,9 +7,13 @@ import (
 	"github.com/techschool/simplebank/util"
 )
 func createRandomUser(t *testing.T) User {
+
+	hashedPassword, err := util.HashPassword(util.RandomString(6))
+	require.NoError(t, err)
+
 	arg := CreateUserParams{
 		Username:       util.RandomOwner(),
-		HashedPassword: "secret",
+		HashedPassword: hashedPassword,
 		FullName:       util.RandomOwner(),
 		Email:          util.RandomEmail(),
 	}
